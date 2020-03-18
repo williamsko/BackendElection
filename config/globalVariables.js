1d6dfa02272edf6f57f013332cb1c1560edee6c6
Alexis babacar Sambou <alexis.sambou@gmailcom>
2020-03-18 20:52:02 +0000

    test pour voir

--- config/globalVariables.js
+++ config/globalVariables.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:'mongodb+srv://lexis:153426slimalexiS@cluster0-naxql.mongodb.net/AjacadElection?retryWrites=true&w=majority',
+    mongoURI:'mongodb+srv://lexis:153426slimalexiS@cluster0-naxql.mongodb.net/Malicratie?retryWrites=true&w=majority',
     mongoURILocal:'mongodb://localhost:27017/Malicratie'
 
 }
